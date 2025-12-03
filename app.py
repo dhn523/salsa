fe03d9f7fce860194aa464123ef14781c5102375
dhn523 <dnandha5231995@gmail.com>
2025-12-02 22:38:17 -0500

    Added a favorites screen with tabs

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from MTAInfo import get_next_trains
 from requests import request
 from flask import request
-from models import db, GTFSStop  # Import your new model
+from models import db, GTFSStop  # Import your new model for DB
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
@@ -16,7 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Initialize database with app
 db.init_app(app)
 
-# Take this out for one time script
+# Take this out for one time script with DB
 # with app.app_context():
 #     db.create_all()
 #     print("Database tables created!")
@@ -24,12 +24,47 @@ db.init_app(app)
 
 @app.route('/')
 def pookie():
-    testing  = True
+    testing  = False
     if testing:
         return render_template('frontPage_test.html')
     else:
         return render_template('frontPage.html')
 
+@app.route('/typeahead')
+def typeahead():
+    return render_template('typeahead_testing.html')
+
+@app.route('/favorites')
+def favorites():
+    return render_template('favorites.html')
+
+@app.route('/api/favorites/train-times/<stop_id>')
+def get_favorite_train_times(stop_id):
+    """Get train times for a favorite stop"""
+    # Define the routes for each stop
+    stop_routes = {
+        'F21N': 'F G',
+        'R28N': 'R W',
+        'A40N': 'A C',
+        'R27S': 'R W',
+        'M16S': 'J M Z'
+    }
+    
+    routes_text = stop_routes.get(stop_id, '')
+    arrivals = get_next_trains(stop_id, 3)
+    
+    if arrivals and len(arrivals) > 0:
+        # Format the data for the frontend
+        train_times = [
+            {
+                'route': arrival[0],
+                'minutes': arrival[1]
+            }
+            for arrival in arrivals
+        ]
+        return jsonify(train_times)
+    else:
+        return jsonify([])
 
 @app.route('/api/stops/search')
 def search_stops():
@@ -47,6 +82,7 @@ def get_all_stops():
     return jsonify([stop.to_dict() for stop in stops])
 
 
+# Most basic display with the information for the Northbound F train at Carroll St
 @app.route('/routes')
 def home_routes():
 
@@ -73,34 +109,12 @@ def home_routes():
         next_trains = []
         next_routes = []
     
-    return render_template('frontPage_routes.html', 
+    return render_template('frontPage_basicRoute.html', 
                          minutes=next_train_minutes,
                          current_route=current_route,
                          next_trains=next_trains,
                          next_routes=next_routes)
 
-
-# @app.route('/')
-# def home():
-#     test = "F21N"
-#     arrivals = get_next_trains(test, 3)  # Get 3 trains instead of just 1
-    
-#     if arrivals and len(arrivals) > 0:
-#         # First train for main display
-#         next_train_minutes = arrivals[0][1]
-        
-#         # Extract just the minutes for the next trains
-#         next_trains = [arrival[1] for arrival in arrivals]
-        
-#         # print(f"All arrivals: {arrivals}")
-#         # print(f"Minutes array: {next_trains}")
-#     else:
-#         next_train_minutes = "N/A"
-#         next_trains = []
-    
-#     return render_template('frontPage_mvp.html', 
-#                          minutes=next_train_minutes, 
-#                          next_trains=next_trains)
 
 @app.route('/test')
 def test():
